554d460262bba2fb1741a2d5f51762e23b3dc399
Gustavo Malano <gmalano@na-sa.com.ar>
2023-02-07 12:22:23 -0300

    eliminado código de coloreado de acuerdo a los planes de inspección
    ya que los planes se superponen. Ahora el coloreado se hace en la interfaz web

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <map>
-#include <array>
 #include <utility>
 #include <filesystem>
 #include <algorithm>
-#include <ctime>
-#include <random>
 #include <cmath>
 #include "csv.h"
 #include "rapidxml.hpp"
@@ -21,43 +17,7 @@ struct tube {
 	float cl_y;
 	float hl_x;
 	float hl_y;
-	std::string color = "white";
-	std::string insp_plan = "";
 };
-
-void read_inspection_plans(std::map<int, tube> &tubes) {
-
-	std::array<std::string, 9> pastel_colors = { "#ffadad", "#ffd6a5",
-			"#fdffb6", "#caffbf", "#9bf6ff", "#a0c4ff", "#bdb2ff", "#ffc6ff",
-			"#ffffbb" };
-	int color = 0;
-
-	std::string path = "./insp_plans";
-	for (const auto &entry : std::filesystem::directory_iterator(path)) {
-		std::cout << "Procesando plan: " << entry.path().filename() << "\n";
-
-		if (!(entry.path().filename().extension() == ".csv")) {
-			continue;
-		}
-
-		// Parse the CSV file to extract the data for the plan
-		io::CSVReader<3, io::trim_chars<' ', '\t'>, io::no_quote_escape<';'> > ip(
-				entry.path());
-		ip.read_header(io::ignore_extra_column, "ROW", "COL", "TUBE");
-		std::string row, col;
-		std::string tube_num;
-
-		while (ip.read_row(row, col, tube_num)) {
-			if (auto pos = tubes.find(std::stoi(tube_num.substr(5))); pos
-					!= tubes.end()) {
-				(*pos).second.color = pastel_colors[color];
-				(*pos).second.insp_plan = entry.path().filename();
-			}
-		}
-		color++;
-		color %= pastel_colors.size();
-	}
-}
 
 int main() {
 	float tube_r = .625 / 2;
@@ -84,11 +44,6 @@ int main() {
 
 	}
 
-//	sort_labels(x_labels);
-//	sort_labels(y_labels);
-
-// Parse the CSV file to extract the data for the plan
-	// read_inspection_plans(tubes);
 	// Search for max x and y distances
 	auto max_x_it = std::max_element(tubes.begin(), tubes.end(),
 			[](auto a, auto b) {
@@ -117,10 +72,10 @@ int main() {
 	svg_node->append_attribute(
 			doc.allocate_attribute("viewBox",
 					doc.allocate_string(
-							("-" + std::to_string(margin_x) + " -" + std::to_string(margin_y) + " "
+							("-" + std::to_string(margin_x) + " -"
+									+ std::to_string(margin_y) + " "
 									+ std::to_string(
-											std::ceil(max_x) + margin_x)
-									+ " "
+											std::ceil(max_x) + margin_x) + " "
 									+ std::to_string(
 											std::ceil(max_y) + margin_y)).c_str())));
 
@@ -128,8 +83,8 @@ int main() {
 	style_node->append_attribute(doc.allocate_attribute("type", "text/css"));
 
 	style_node->value(
-			".tube {stroke: black; stroke-width: 0.02; } "
-					".tube_num { text-anchor: middle; alignment-baseline: middle; font-family: sans-serif; font-size: 0.25px; fill: black  }"
+			".tube {stroke: black; stroke-width: 0.02; fill: none;} "
+					".tube_num { text-anchor: middle; alignment-baseline: middle; font-family: sans-serif; font-size: 0.25px; fill: black;}"
 					".label { text-anchor: middle; alignment-baseline: middle; font-family: sans-serif; font-size: 0.25px; fill: red;}");
 
 	svg_node->append_node(style_node);
@@ -143,15 +98,18 @@ int main() {
 				doc.allocate_string(label.c_str()));
 		label_node->append_attribute(
 				doc.allocate_attribute("x",
+						doc.allocate_string((std::to_string(coord)).c_str())));
+		label_node->append_attribute(
+				doc.allocate_attribute("y",
 						doc.allocate_string(
-								(std::to_string(coord)).c_str())));
-		label_node->append_attribute(
-				doc.allocate_attribute("y", doc.allocate_string(("-" + std::to_string(margin_y * 0.75)).c_str())));
-		label_node->append_attribute(
-				doc.allocate_attribute("class", "label"));
+								("-" + std::to_string(margin_y * 0.75)).c_str())));
+		label_node->append_attribute(doc.allocate_attribute("class", "label"));
 
 		label_node->append_attribute(
-				doc.allocate_attribute("transform", doc.allocate_string(("rotate(270,"+ std::to_string(coord) + ", -" + std::to_string(margin_y * 0.75) + ")").c_str())));
+				doc.allocate_attribute("transform",
+						doc.allocate_string(
+								("rotate(270," + std::to_string(coord) + ", -"
+										+ std::to_string(margin_y * 0.75) + ")").c_str())));
 
 		svg_node->append_node(label_node);
 	}
@@ -164,11 +122,10 @@ int main() {
 		label_node->append_attribute(
 				doc.allocate_attribute("x",
 						doc.allocate_string(
-								("-" +  std::to_string(margin_x * 0.75)).c_str())));
+								("-" + std::to_string(margin_x * 0.75)).c_str())));
 		label_node->append_attribute(
 				doc.allocate_attribute("y",
-						doc.allocate_string(
-								(std::to_string(coord)).c_str())));
+						doc.allocate_string((std::to_string(coord)).c_str())));
 		label_node->append_attribute(doc.allocate_attribute("class", "label"));
 
 		svg_node->append_node(label_node);
@@ -195,16 +152,12 @@ int main() {
 		tube_node->append_attribute(
 				doc.allocate_attribute("r",
 						doc.allocate_string(std::to_string(tube_r).c_str())));
-		tube_node->append_attribute(
-				doc.allocate_attribute("fill",
-						doc.allocate_string(tube.color.c_str())));
 		tube_node->append_attribute(doc.allocate_attribute("class", "tube"));
 
 		auto tooltip_node = doc.allocate_node(rapidxml::node_element, "title");
 		tooltip_node->value(
 				doc.allocate_string(
-						("X=" + tube.x_label + " Y=" + tube.y_label + " "
-								+ tube.insp_plan).c_str()));
+						("X=" + tube.x_label + " Y=" + tube.y_label).c_str()));
 		tube_group_node->append_node(tooltip_node);
 		tube_group_node->append_node(tube_node);
 		auto number_node = doc.allocate_node(rapidxml::node_element, "text",
@@ -234,4 +187,3 @@ int main() {
 
 	return 0;
 }
-
