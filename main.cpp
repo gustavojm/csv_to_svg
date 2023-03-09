0da8e131b89d52d689bb6e6fdd4bcd3cd065a6e6
Gustavo Malano <gmalano@na-sa.com.ar>
2023-03-09 15:00:43 -0300

    removidos algunas referencias y cambiadas a punteros...

--- main.cpp
+++ main.cpp
@@ -19,16 +19,16 @@ struct tube {
     float y;
 };
 
-void append_attributes(rapidxml::xml_document<char> &doc,
+void append_attributes(rapidxml::xml_document<char> *doc,
         rapidxml::xml_node<char> *node,
         std::map<std::string, std::string> attrs) {
     if (node) {
 
         for (auto attr : attrs) {
             node->append_attribute(
-                    doc.allocate_attribute(
-                            doc.allocate_string(attr.first.c_str()),
-                            doc.allocate_string(attr.second.c_str())));
+                    doc->allocate_attribute(
+                            doc->allocate_string(attr.first.c_str()),
+                            doc->allocate_string(attr.second.c_str())));
         }
     }
 }
@@ -37,7 +37,7 @@ void add_dashed_line(rapidxml::xml_node<char> *parent_node, float x1, float y1,
         float x2, float y2) {
     auto doc = parent_node->document();
     auto line_node = doc->allocate_node(rapidxml::node_element, "line");
-    append_attributes(*doc, line_node, { { "x1", std::to_string(x1) }, { "y1",
+    append_attributes(doc, line_node, { { "x1", std::to_string(x1) }, { "y1",
             std::to_string(y1) }, { "x2", std::to_string(x2) }, { "y2",
             std::to_string(y2) }, { "stroke", "gray" },
             { "stroke-width", "0.02" }, { "stroke-dasharray", "0.2, 0.1" }, });
@@ -51,25 +51,25 @@ rapidxml::xml_node<char>* add_label(rapidxml::xml_node<char> *parent_node,
     auto label_node = doc->allocate_node(rapidxml::node_element, "text",
             doc->allocate_string(label));
 
-    append_attributes(*doc, label_node, { { "x", std::to_string(x) }, { "y",
+    append_attributes(doc, label_node, { { "x", std::to_string(x) }, { "y",
             std::to_string(y) }, { "class", "label" }, });
 
     return label_node;
 }
 
-rapidxml::xml_node<char>* add_tube(const rapidxml::xml_node<char> &parent_node,
+rapidxml::xml_node<char>* add_tube(const rapidxml::xml_node<char> *parent_node,
         float x, float y, float radius, std::string id,
         const std::string &x_label, const std::string &y_label) {
-    auto doc = parent_node.document();
+    auto doc = parent_node->document();
     auto tube_group_node = doc->allocate_node(rapidxml::node_element, "g");
-    append_attributes(*doc, tube_group_node,
+    append_attributes(doc, tube_group_node,
             { { "id", doc->allocate_string(id.c_str()) },
                     { "data-col", x_label }, { "data-row", y_label }
 
             });
 
     auto tube_node = doc->allocate_node(rapidxml::node_element, "circle");
-    append_attributes(*doc, tube_node, { { "cx", std::to_string(x) }, { "cy",
+    append_attributes(doc, tube_node, { { "cx", std::to_string(x) }, { "cy",
             std::to_string(y) }, { "r", std::to_string(radius) }, { "class",
             "tube" }, });
 
@@ -82,7 +82,7 @@ rapidxml::xml_node<char>* add_tube(const rapidxml::xml_node<char> &parent_node,
     auto number_node = doc->allocate_node(rapidxml::node_element, "text",
             doc->allocate_string(id.substr(2).c_str()));
     //number_node->value();
-    append_attributes(*doc, number_node, { { "class", "tube_num" }, { "x",
+    append_attributes(doc, number_node, { { "class", "tube_num" }, { "x",
             std::to_string(x) }, { "y", std::to_string(y) }, });
 
     tube_group_node->append_node(number_node);
@@ -115,14 +115,12 @@ int main(int argc, char *argv[]) {
                 po::value<float>(&min_x)->default_value(0),
                 "viewBox X minimum coord");
         settings_desc.add_options()("width",
-                po::value<float>(&width)->default_value(0),
-                "viewBoxWidth");
+                po::value<float>(&width)->default_value(0), "viewBoxWidth");
         settings_desc.add_options()("min_y",
                 po::value<float>(&min_y)->default_value(0),
                 "viewBox Y minimum coord");
         settings_desc.add_options()("height",
-                po::value<float>(&height)->default_value(0),
-                "viewBox Height");
+                po::value<float>(&height)->default_value(0), "viewBox Height");
         settings_desc.add_options()("font_size",
                 po::value<std::string>(&font_size)->default_value("0.25"),
                 "Number font size in px");
@@ -132,21 +130,21 @@ int main(int argc, char *argv[]) {
         settings_desc.add_options()("y_labels",
                 po::value<std::string>(&y_labels_param)->default_value("0"),
                 "Where to locate x axis labels, can use several coords separated by space");
-        settings_desc.add_options()
-              ("help,h", "Help screen");    // what an strange syntax...
-              // ("config", po::value<std::string>(), "Config file");
+        settings_desc.add_options()("help,h", "Help screen"); // what an strange syntax...
+        // ("config", po::value<std::string>(), "Config file");
         po::variables_map vm;
 
         po::store(po::parse_command_line(argc, argv, settings_desc), vm);
         std::filesystem::path config = std::filesystem::path("config.ini");
         if (std::filesystem::exists(config)) {
             std::ifstream config_is = std::ifstream(config);
-            po::store(po::parse_config_file(config_is, settings_desc, true), vm);
+            po::store(po::parse_config_file(config_is, settings_desc, true),
+                    vm);
         }
         po::notify(vm);
 
         if (vm.count("help")) {
-              std::cout << settings_desc << '\n';
+            std::cout << settings_desc << '\n';
         }
 
         boost::split(x_labels_coords, x_labels_param, boost::is_any_of(" "));
@@ -174,55 +172,54 @@ int main(int argc, char *argv[]) {
             tubes.insert( { std::string(std::string("cl") + tube_id.substr(5)),
                     { x_label, y_label, cl_x, -cl_y } });
             x_labels.insert(std::make_pair(x_label, cl_x));
-            y_labels.insert(
-                    std::make_pair(y_label, -cl_y));
+            y_labels.insert(std::make_pair(y_label, -cl_y));
         }
 
         if (leg == "hot" || leg == "both") {
             tubes.insert( { std::string(std::string("hl") + tube_id.substr(5)),
                     { x_label, y_label, hl_x, -hl_y } });
             x_labels.insert(std::make_pair(x_label, hl_x));
-            y_labels.insert(
-                    std::make_pair(y_label, -hl_y));
+            y_labels.insert(std::make_pair(y_label, -hl_y));
         }
     }
 
     // Create the SVG document
-    rapidxml::xml_document<char> doc;
-    auto svg_node = doc.allocate_node(rapidxml::node_element, "svg");
+    rapidxml::xml_document<char> document;
+    rapidxml::xml_document<char> *doc = &document;
+    auto svg_node = doc->allocate_node(rapidxml::node_element, "svg");
 
     append_attributes(doc, svg_node,
             { { "xmlns", "http://www.w3.org/2000/svg" }, { "version", "1.1" }, {
-                    "id", "tubesheet_svg" }, { "viewBox",
-                      std::to_string(min_x) + " "
-                    + std::to_string(min_y) + " "
-                    + std::to_string(width) + " "
-                    + std::to_string(height) } });
+                    "id", "tubesheet_svg" }, { "viewBox", std::to_string(min_x)
+                    + " " + std::to_string(min_y) + " " + std::to_string(width)
+                    + " " + std::to_string(height) } });
 
-    auto style_node = doc.allocate_node(rapidxml::node_element, "style");
+    auto style_node = doc->allocate_node(rapidxml::node_element, "style");
     append_attributes(doc, style_node, { { "type", "text/css" } });
 
-    std::string style = std::string(".tube {stroke: black; stroke-width: 0.02; fill: white;} ") +
-            ".tube_num { text-anchor: middle; alignment-baseline: middle; font-family: sans-serif; font-size: " + font_size + "px; fill: black;}"
-            ".label { text-anchor: middle; alignment-baseline: middle; font-family: sans-serif; font-size: 0.25px; fill: red;}";
+    std::string style =
+            std::string(
+                    ".tube {stroke: black; stroke-width: 0.02; fill: white;} ")
+                    + ".tube_num { text-anchor: middle; alignment-baseline: middle; font-family: sans-serif; font-size: "
+                    + font_size
+                    + "px; fill: black;}"
+                            ".label { text-anchor: middle; alignment-baseline: middle; font-family: sans-serif; font-size: 0.25px; fill: red;}";
 
     style_node->value(style.c_str());
 
     svg_node->append_node(style_node);
-    doc.append_node(svg_node);
+    doc->append_node(svg_node);
 
-    add_dashed_line(svg_node, min_x, 0,
-            min_x + width, 0);
-    add_dashed_line(svg_node, 0, min_y, 0,
-            min_y + height);
+    add_dashed_line(svg_node, min_x, 0, min_x + width, 0);
+    add_dashed_line(svg_node, 0, min_y, 0, min_y + height);
 
     for (auto coord_y : x_labels_coords) {
         for (auto [label, coord] : x_labels) {
             auto label_x = add_label(svg_node, coord, std::stof(coord_y),
                     label.c_str());
             append_attributes(doc, label_x,
-                    { { "transform", "rotate(270," + std::to_string(coord) + ", "
-                            + coord_y + ")" }, });
+                    { { "transform", "rotate(270," + std::to_string(coord)
+                            + ", " + coord_y + ")" }, });
             svg_node->append_node(label_x);
         }
     }
@@ -239,7 +236,7 @@ int main(int argc, char *argv[]) {
     for (const auto &tube_pair : tubes) {
         auto tube = tube_pair.second;
 
-        auto tube_node = add_tube(*svg_node, tube.x, tube.y, tube_r,
+        auto tube_node = add_tube(svg_node, tube.x, tube.y, tube_r,
                 tube_pair.first, tube.x_label, tube.y_label);
         svg_node->append_node(tube_node);
 
@@ -247,7 +244,7 @@ int main(int argc, char *argv[]) {
 
     // Write the SVG document to a file
     std::ofstream file("tubesheet.svg");
-    file << doc;
+    file << document;
     file.close();
 
     return 0;
